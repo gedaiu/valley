644de717926d3a88150fe0f3501856a5e5c1d2c1
Szabo Bogdan <bogdan.szabo@altom.ro>
2017-10-29 00:41:07 +0300

    aes encryption

--- source/valley/peer.d
+++ source/valley/peer.d
@@ -9,21 +9,117 @@ import std.conv;
 import std.exception;
 import std.string;
 
+import deimos.openssl.rand;
 import deimos.openssl.bio;
 import deimos.openssl.x509;
 import deimos.openssl.pem;
 import deimos.openssl.rsa;
 import deimos.openssl.err;
+import deimos.openssl.ssl;
 
 alias OnMessageEvent = void delegate(Message message) nothrow;
 
-struct Message {
+shared static this()
+{
+  SSL_load_error_strings();
+  ERR_load_BIO_strings();
+  OpenSSL_add_all_algorithms();
+}
+
+class AES
+{
+  import deimos.openssl.aes;
+  import deimos.openssl.rand;
+
+  private
+  {
+    EVP_CIPHER_CTX ctx;
+  }
+
+  const
+  {
+    ubyte[16] key;
+    ubyte[16] iv;
+  }
+
+  this()
+  {
+    ubyte[16] data;
+    EVP_CIPHER_CTX_init(&ctx);
+    EVP_CipherInit_ex(&ctx, EVP_aes_128_cbc(), null, null, null, 1);
+
+    enforce(EVP_CIPHER_CTX_key_length(&ctx) == data.length);
+    enforce(EVP_CIPHER_CTX_iv_length(&ctx) == data.length);
+
+    auto result = RAND_bytes(data.ptr, data.length);
+    enforce(result == 1, ERR_error_string(ERR_get_error(), null).fromStringz);
+    key = data.dup;
+
+    result = RAND_bytes(data.ptr, data.length);
+    enforce(result == 1, ERR_error_string(ERR_get_error(), null).fromStringz);
+    iv = data.dup;
+  }
+
+  auto encrypt(string data)
+  {
+    EVP_CipherInit_ex(&ctx, null, null, key.dup.ptr, iv.dup.ptr, 1);
+
+    data ~= "\0";
+    ubyte[] encrypted;
+    int encryptedLen = ((data.length / key.length).to!int + 1) * key.length.to!int;
+    int dataLen = data.length.to!int;
+    encrypted.length = encryptedLen;
+
+    const(ubyte)* dataCopy = cast(const(ubyte)*) data.dup.toStringz;
+
+    auto result = EVP_CipherUpdate(&ctx, encrypted.ptr, &encryptedLen, dataCopy, dataLen);
+    enforce(result == 1, ERR_error_string(ERR_get_error(), null).fromStringz);
+
+    result = EVP_CipherFinal_ex(&ctx, encrypted.ptr, &encryptedLen);
+    enforce(result == 1, ERR_error_string(ERR_get_error(), null).fromStringz);
+
+    return encrypted;
+  }
+
+  string decrypt(ubyte[] data)
+  {
+    EVP_CipherInit_ex(&ctx, null, null, key.dup.ptr, null, 0);
+
+    ubyte[] decrypted;
+    decrypted.length = data.length;
+
+    int decryptedLen = data.length.to!int;
+    int dataLen = data.length.to!int;
+
+    auto result = EVP_CipherUpdate(&ctx, decrypted.ptr, &decryptedLen, data.ptr, dataLen);
+    enforce(result == 1, ERR_error_string(ERR_get_error(), null).fromStringz);
+
+    result = EVP_CipherFinal_ex(&ctx, decrypted.ptr, &decryptedLen);
+    enforce(result == 1, ERR_error_string(ERR_get_error(), null).fromStringz);
+
+    return (cast(char*) decrypted.ptr).fromStringz.to!string;
+  }
+}
+
+/// AES should encrypt a text
+unittest
+{
+  auto aes = new AES();
+
+  auto result = aes.encrypt("some text");
+
+  aes.decrypt(result).should.equal("some text");
+}
+
+struct Message
+{
   string destinationId;
   string data;
   string sourceId;
 }
 
-struct Packet {
+struct Packet
+{
   string sourceId;
   string destinationId;
 
@@ -31,88 +127,107 @@ struct Packet {
   string signature;
 }
 
-extern(C) {
-  X509 *PEM_read_bio_X509(BIO *bp, X509 **x, pem_password_cb* callback, void *u);
+extern (C)
+{
+  X509* PEM_read_bio_X509(BIO* bp, X509** x, pem_password_cb* callback, void* u);
 }
 
-class Certificate {
-  private {
+class Certificate
+{
+  private
+  {
     rsa_st* rsaPubKey;
-    BIO *bio;
-    X509 *certificate;
+    BIO* bio;
+    X509* certificate;
   }
 
-  this(string fileName) {
+  this(string fileName)
+  {
     string certificateString = readText(fileName);
 
-    EVP_PKEY *pubkey;
+    EVP_PKEY* pubkey;
     bio = BIO_new(BIO_s_mem());
 
-    auto lTemp = BIO_write(bio, cast(const(void)*) certificateString.ptr, certificateString.length.to!int);
+    auto lTemp = BIO_write(bio, cast(const(void)*) certificateString.ptr,
+        certificateString.length.to!int);
     enforce(lTemp == certificateString.length);
 
     certificate = PEM_read_bio_X509(bio, null, null, null);
     pubkey = X509_get_pubkey(certificate);
-    scope(exit) EVP_PKEY_free(pubkey);
+    scope (exit)
+      EVP_PKEY_free(pubkey);
 
     rsaPubKey = EVP_PKEY_get1_RSA(pubkey);
   }
 
-  ~this() {
+  ~this()
+  {
     X509_free(certificate);
     BIO_vfree(bio);
     RSA_free(rsaPubKey);
   }
 
-  ubyte[] encrypt(string message) {
+  ubyte[] encrypt(string message)
+  {
+    auto aes = new AES();
     ubyte[] encryptedData;
 
     encryptedData.length = RSA_size(rsaPubKey);
-    auto temp = RSA_public_encrypt(message.length.to!int, (cast(ubyte[]) message).ptr, encryptedData.ptr, rsaPubKey, RSA_PKCS1_OAEP_PADDING);
+    auto temp = RSA_public_encrypt(message.length.to!int, (cast(ubyte[]) message)
+        .ptr, encryptedData.ptr, rsaPubKey, RSA_PKCS1_OAEP_PADDING);
 
     return encryptedData;
   }
 }
 
-Packet toPacket(Message message, string sourceId) {
+Packet toPacket(Message message, string sourceId)
+{
   auto certificate = new Certificate("keys/server.crt");
 
   return Packet(sourceId, message.destinationId, certificate.encrypt(message.data));
 }
 
-class PrivateKey {
+class PrivateKey
+{
 
-  private {
-    RSA *rsa_privkey;
-    BIO *bio;
+  private
+  {
+    RSA* rsa_privkey;
+    BIO* bio;
   }
 
-  this(string fileName) {
+  this(string fileName)
+  {
     string certificateString = readText("keys/server.key");
 
     bio = BIO_new(BIO_s_mem());
 
-    auto lTemp = BIO_write(bio, cast(const(void)*) certificateString.ptr, certificateString.length.to!int);
-    enforce(lTemp == certificateString.length);
+    auto lTemp = BIO_write(bio, cast(const(void)*) certificateString.ptr,
+        certificateString.length.to!int);
+    enforce(lTemp == certificateString.length, ERR_error_string(ERR_get_error(), null).fromStringz);
 
     rsa_privkey = PEM_read_bio_RSAPrivateKey(bio, &rsa_privkey, null, null);
   }
 
-  ~this() {
+  ~this()
+  {
     BIO_vfree(bio);
     RSA_free(rsa_privkey);
   }
 
-  string decrypt(ubyte[] data) {
+  string decrypt(ubyte[] data)
+  {
     ubyte[] decrypted;
     decrypted.length = RSA_size(rsa_privkey);
 
-    auto resultDecrypt = RSA_private_decrypt(data.length.to!int, data.ptr, decrypted.ptr, rsa_privkey, RSA_PKCS1_OAEP_PADDING);
-    enforce(resultDecrypt > 0);
+    auto result = RSA_private_decrypt(data.length.to!int, data.ptr,
+        decrypted.ptr, rsa_privkey, RSA_PKCS1_OAEP_PADDING);
+    enforce(result != -1, ERR_error_string(ERR_get_error(), null).fromStringz);
 
     auto pos = decrypted.countUntil(0);
-    if(pos != -1) {
-      decrypted = decrypted[0..pos];
+    if (pos != -1)
+    {
+      decrypted = decrypted[0 .. pos];
     }
 
     return decrypted.assumeUTF;
@@ -120,7 +235,8 @@ class PrivateKey {
 }
 
 /// It should convert a Message to a Packet
-unittest {
+unittest
+{
   auto message = Message("destinationId", "some data");
   auto packet = message.toPacket("sourceId");
 
@@ -130,14 +246,16 @@ unittest {
   packet.signature.length.should.equal(0);
 }
 
-Message decrypt(Packet packet) {
+Message decrypt(Packet packet)
+{
   auto privateKey = new PrivateKey("keys/server.key");
 
   return Message(packet.destinationId, privateKey.decrypt(packet.data), packet.sourceId);
 }
 
 /// It should decode a Packet to a message
-unittest {
+unittest
+{
   auto packet = Message("destinationId", "some data").toPacket("sourceId");
   auto message = packet.decrypt();
 
@@ -146,20 +264,48 @@ unittest {
   message.data.should.equal("some data");
 }
 
-class PeerCollection {
+/// It should decode a long Packet to a message
+unittest
+{
+  import std.random;
+
+  auto rnd = Random(42);
+
+  string data;
+  foreach (i; 0 .. 1000)
+  {
+    data ~= uniform(0, 9, rnd).to!string;
+  }
+
+  data.writeln;
+
+  auto packet = Message("destinationId", data).toPacket("sourceId");
+  auto message = packet.decrypt();
+
+  message.sourceId.should.equal("sourceId");
+  message.destinationId.should.equal("destinationId");
+  message.data.should.equal(data);
+}
+
+class PeerCollection
+{
   ValleyPeer[] peers;
 
   alias peers this;
 
-  ValleyPeer relayTo(string peerId) {
-    struct Pair {
+  ValleyPeer relayTo(string peerId)
+  {
+    struct Pair
+    {
       ValleyPeer peer;
       int distance;
     }
 
-    auto knownPeer = peers.map!(a => Pair(a, a.distanceTo(peerId))).array.sort!"a.distance < b.distance";
+    auto knownPeer = peers.map!(a => Pair(a, a.distanceTo(peerId)))
+      .array.sort!"a.distance < b.distance";
 
-    if(knownPeer.length == 0) {
+    if (knownPeer.length == 0)
+    {
       throw new Exception("No peer.");
     }
 
@@ -167,37 +313,47 @@ class PeerCollection {
   }
 }
 
-class ValleyPeer {
+class ValleyPeer
+{
   string id;
   PeerCollection peers;
 
-  private {
+  private
+  {
     OnMessageEvent[] onMessageEvents;
   }
 
-  this() {
+  this()
+  {
     peers = new PeerCollection;
   }
 
-  void onMessage(OnMessageEvent event) {
+  void onMessage(OnMessageEvent event)
+  {
     onMessageEvents ~= event;
   }
 
-  int distanceTo(string peerId) {
-    if(peerId == id) {
+  int distanceTo(string peerId)
+  {
+    if (peerId == id)
+    {
       return 0;
     }
 
-    if(peers.map!"a.id".canFind(peerId)) {
+    if (peers.map!"a.id".canFind(peerId))
+    {
       return 1;
     }
 
     return int.max;
   }
 
-  void send(Message message) {
-    if(message.destinationId == id) {
-      foreach(event; onMessageEvents) {
+  void send(Message message)
+  {
+    if (message.destinationId == id)
+    {
+      foreach (event; onMessageEvents)
+      {
         event(message);
       }
 
@@ -209,12 +365,14 @@ class ValleyPeer {
   }
 }
 
-version(unittest) {
+version (unittest)
+{
   import fluent.asserts;
 }
 
 /// It should send a message using a relay peer
-unittest {
+unittest
+{
   auto client1 = new ValleyPeer;
   client1.id = "client1";
 
@@ -229,12 +387,17 @@ unittest {
 
   bool gotMessage;
 
-  void messageHandler(Message message) nothrow {
-    try {
+  void messageHandler(Message message) nothrow
+  {
+    try
+    {
       message.destinationId.should.equal(client3.id);
       message.data.should.equal("hello");
       gotMessage = true;
-    } catch(Exception e) { }
+    }
+    catch (Exception e)
+    {
+    }
   }
 
   client3.onMessage(&messageHandler);
@@ -248,7 +411,8 @@ unittest {
 
 /// It should send a message using a relay peer without sending to
 /// peers that don't have a link to the destination peer
-unittest {
+unittest
+{
   auto client1 = new ValleyPeer;
   client1.id = "client1";
 
@@ -261,12 +425,13 @@ unittest {
   auto client4 = new ValleyPeer;
   client4.id = "client4";
 
-  client1.peers ~= [ client4, client2 ];
+  client1.peers ~= [client4, client2];
   client2.peers ~= client3;
 
   bool gotMessage;
 
-  void messageHandler(Message message) nothrow {
+  void messageHandler(Message message) nothrow
+  {
     gotMessage = true;
   }
 
