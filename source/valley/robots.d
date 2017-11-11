3e07b9f8e2617db771f5dae8b6fd284e77649bb2
Szabo Bogdan <bogdan.szabo@altom.ro>
2017-11-11 02:38:36 +0200

    parse a simple robots

--- source/valley/robots.d
+++ source/valley/robots.d
@@ -54,3 +54,21 @@ unittest {
   robots.agents["*"].disallow.length.should.equal(1);
   robots.agents["*"].disallow[0].should.equal("");
 }
+
+/// Parsing robots.txt with two user agents
+unittest {
+  auto robots = Robots(`User-agent: a
+Disallow: /private/
+
+User-agent: b
+Disallow: /
+`);
+
+  robots.agents.length.should.equal(1);
+  robots.agents.keys.should.contain([ "a", "b" ]);
+  robots.agents["a"].disallow.length.should.equal(1);
+  robots.agents["a"].disallow[0].should.equal("/private/");
+
+  robots.agents["b"].disallow.length.should.equal(1);
+  robots.agents["b"].disallow[0].should.equal("/");
+}
