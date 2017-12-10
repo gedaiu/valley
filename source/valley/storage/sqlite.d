871cde2bd6dcc717743600e6e590adda7455207e
Szabo Bogdan <bogdan.szabo@altom.ro>
2017-12-10 12:41:48 +0200

    link the keywords with pages

--- source/valley/storage/sqlite.d
+++ source/valley/storage/sqlite.d
@@ -17,31 +17,26 @@ version (unittest)
 void setupSqliteDb(string fileName) {
   auto db = Database(fileName);
   db.run(`CREATE TABLE pages (
+          id           INTEGER PRIMARY KEY autoincrement,
           title        TEXT NOT NULL,
           location     TEXT NOT NULL,
           description  TEXT NOT NULL,
-          time         INT NOT NULL,
-          type         INT NOT NULL
+          time         INTEGER NOT NULL,
+          type         INTEGER NOT NULL
         )`);
 
   db.run(`CREATE TABLE keywords (
-          keyword        TEXT NOT NULL
+          id           INTEGER primary key autoincrement,
+          keyword      TEXT NOT NULL
         )`);
+
+  db.run(`CREATE TABLE keywordLinks (
+    pageId        INTEGER NOT NULL,
+    keywordId     INTEGER NOT NULL
+  )`);
 
   db.close;
 }
-/*
-  string title;
-  URI location;
-  string description;
-  SysTime time;
-
-  URI[] relations;
-  Badge[] badges;
-  string[] keywords;
-
-  InformationType type;
-*/
 
 class SQLiteStorage : Storage
 {
@@ -49,6 +44,8 @@ class SQLiteStorage : Storage
     Database db;
     Statement insertPage;
     Statement insertKeyword;
+    Statement insertKeywordLinks;
+    Statement lastInsertId;
   }
 
   this(string fileName) {
@@ -61,7 +58,15 @@ class SQLiteStorage : Storage
                                         VALUES (:title, :location, :description, :time, :type )");
 
     insertKeyword = db.prepare("INSERT INTO keywords (keyword) VALUES (:keyword) ");
+    insertKeywordLinks = db.prepare("INSERT INTO keywordLinks (keywordId, pageId) VALUES (:keywordId, :pageId) ");
+    lastInsertId = db.prepare("SELECT last_insert_rowid()");
+  }
 
+  ulong getLastId() {
+    ulong id = lastInsertId.execute.oneValue!ulong;
+    lastInsertId.reset;
+
+    return id;
   }
 
   void add(PageData data)
@@ -75,11 +80,24 @@ class SQLiteStorage : Storage
     insertPage.execute;
     insertPage.reset;
 
+    auto pageId = getLastId;
+    ulong[] keywords;
+
     foreach(keyword; data.keywords) {
       insertKeyword.bind(":keyword", keyword);
 
       insertKeyword.execute;
       insertKeyword.reset;
+
+      keywords ~= getLastId;
+    }
+
+    foreach(keywordId; keywords) {
+      insertKeywordLinks.bind(":keywordId", keywordId);
+      insertKeywordLinks.bind(":pageId", pageId);
+
+      insertKeywordLinks.execute;
+      insertKeywordLinks.reset;
     }
   }
 
@@ -91,6 +109,8 @@ class SQLiteStorage : Storage
   void close() {
     insertPage.finalize;
     insertKeyword.finalize;
+    insertKeywordLinks.finalize;
+    lastInsertId.finalize;
 
     db.close;
   }
@@ -98,6 +118,10 @@ class SQLiteStorage : Storage
 
 /// SQLiteStorage should store a page in the db
 unittest {
+  if("data.db".exists) {
+    "data.db".remove;
+  }
+
   auto storage = new SQLiteStorage("data.db");
 
   scope(exit) {
@@ -118,7 +142,7 @@ unittest {
 
     [ URI("http://example.com/page1"), URI("http://example.com/page2") ],
     badges,
-    [ "some", "description" ],
+    [ "some", "keywords" ],
 
     InformationType.webImage
   );
@@ -132,6 +156,7 @@ unittest {
 
   int found = 0;
   foreach (Row row; statement.execute) {
+    row["id"].as!int.should.equal(1);
     row["title"].as!string.should.equal("some title");
     row["location"].as!string.should.equal(data.location.toString);
     row["description"].as!string.should.equal("some description");
@@ -147,9 +172,25 @@ unittest {
 
   string[] keywords;
   foreach (Row row; statement.execute) {
-    keywords ~= row["keyword"].as!string;
+    keywords ~= row["id"].as!string ~ "." ~ row["keyword"].as!string;
   }
 
   statement.finalize;
-  keywords.should.containOnly([ "some", "description" ]);
+  keywords.should.containOnly([ "1.some", "2.keywords" ]);
+
+  // Prepare an SELECT statement for the keywordLinkks
+  statement = db.prepare("SELECT * FROM keywordLinks");
+
+  string[] keywordLinks;
+  foreach (Row row; statement.execute) {
+    keywordLinks ~= row["pageId"].as!string ~ "." ~ row["keywordId"].as!string;
+  }
+
+  statement.finalize;
+  keywordLinks.should.containOnly([ "1.1", "1.2" ]);
+
+  /// Badges
+
+  /// Relations
+
 }
