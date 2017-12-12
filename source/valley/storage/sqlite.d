465566a2468a3ab8dc958400a50f4d7c8b2bd07d
Szabo Bogdan <bogdan.szabo@altom.ro>
2017-12-12 19:41:41 +0200

    insert page links

--- source/valley/storage/sqlite.d
+++ source/valley/storage/sqlite.d
@@ -18,9 +18,9 @@ void setupSqliteDb(string fileName) {
   auto db = Database(fileName);
   db.run(`CREATE TABLE pages (
           id           INTEGER PRIMARY KEY autoincrement,
-          title        TEXT NOT NULL,
+          title        TEXT,
           location     TEXT NOT NULL,
-          description  TEXT NOT NULL,
+          description  TEXT,
           time         INTEGER NOT NULL,
           type         INTEGER NOT NULL
         )`);
@@ -35,6 +35,19 @@ void setupSqliteDb(string fileName) {
     keywordId     INTEGER NOT NULL
   )`);
 
+  db.run(`CREATE TABLE badges (
+          id           INTEGER primary key autoincrement,
+          pageId       INTEGER NOT NULL,
+          type         INTEGER NOT NULL,
+          signature    BLOB NOT NULL
+        )`);
+
+  db.run(`CREATE TABLE links (
+          id              INTEGER primary key autoincrement,
+          pageId          INTEGER NOT NULL,
+          destinationId   INTEGER NOT NULL
+        )`);
+
   db.close;
 }
 
@@ -45,7 +58,11 @@ class SQLiteStorage : Storage
     Statement insertPage;
     Statement insertKeyword;
     Statement insertKeywordLinks;
+    Statement insertBadge;
+    Statement insertLink;
     Statement lastInsertId;
+
+    Statement selectPage;
   }
 
   this(string fileName) {
@@ -58,7 +75,12 @@ class SQLiteStorage : Storage
                                         VALUES (:title, :location, :description, :time, :type )");
 
     insertKeyword = db.prepare("INSERT INTO keywords (keyword) VALUES (:keyword) ");
+    insertBadge = db.prepare("INSERT INTO badges (pageId, type, signature) VALUES (:pageId, :type, :signature) ");
     insertKeywordLinks = db.prepare("INSERT INTO keywordLinks (keywordId, pageId) VALUES (:keywordId, :pageId) ");
+    insertLink = db.prepare("INSERT INTO links (pageId, destinationId) VALUES (:pageId, :destinationId) ");
+
+    selectPage = db.prepare("SELECT * FROM pages WHERE location = :location");
+
     lastInsertId = db.prepare("SELECT last_insert_rowid()");
   }
 
@@ -69,7 +91,63 @@ class SQLiteStorage : Storage
     return id;
   }
 
-  void add(PageData data)
+  ulong addKeyword(string value) {
+    insertKeyword.bind(":keyword", value);
+    insertKeyword.execute;
+    insertKeyword.reset;
+
+    return getLastId;
+  }
+
+  ulong addBadge(ulong pageId, BadgeType type, ubyte[] signature) {
+    insertBadge.bind(":pageId", pageId);
+    insertBadge.bind(":type", type);
+    insertBadge.bind(":signature", signature);
+    insertBadge.execute;
+    insertBadge.reset;
+
+    return getLastId;
+  }
+
+  ulong addKeywordLink(ulong pageId, ulong keywordId) {
+    insertKeywordLinks.bind(":keywordId", keywordId);
+    insertKeywordLinks.bind(":pageId", pageId);
+
+    insertKeywordLinks.execute;
+    insertKeywordLinks.reset;
+
+    return getLastId;
+  }
+
+  void add(PageData data) {
+    addPage(data);
+  }
+
+  ulong getPageId(URI location) {
+    selectPage.bind(":location", location.toString);
+    scope(exit) selectPage.reset;
+
+    auto result = selectPage.execute;
+
+    if(result.empty) {
+      return addPage(PageData("", location));
+    }
+
+    assert(false);
+  }
+
+  ulong addLink(ulong pageId, URI location) {
+    auto destinationId = getPageId(location);
+
+    insertLink.bind(":pageId", pageId);
+    insertLink.bind(":destinationId", destinationId);
+    insertLink.execute;
+    insertLink.reset;
+
+    return getLastId;
+  }
+
+  ulong addPage(PageData data)
   {
     insertPage.bind(":title", data.title);
     insertPage.bind(":location", data.location.toString);
@@ -84,21 +162,22 @@ class SQLiteStorage : Storage
     ulong[] keywords;
 
     foreach(keyword; data.keywords) {
-      insertKeyword.bind(":keyword", keyword);
-
-      insertKeyword.execute;
-      insertKeyword.reset;
-
-      keywords ~= getLastId;
+      keywords ~= addKeyword(keyword);
     }
 
     foreach(keywordId; keywords) {
-      insertKeywordLinks.bind(":keywordId", keywordId);
-      insertKeywordLinks.bind(":pageId", pageId);
-
-      insertKeywordLinks.execute;
-      insertKeywordLinks.reset;
+      addKeywordLink(pageId, keywordId);
     }
+
+    foreach(badge; data.badges) {
+      addBadge(pageId, badge.type, badge.signature);
+    }
+
+    foreach(location; data.relations) {
+      addLink(pageId, location);
+    }
+
+    return pageId;
   }
 
   PageData[] query(string data)
@@ -111,6 +190,9 @@ class SQLiteStorage : Storage
     insertKeyword.finalize;
     insertKeywordLinks.finalize;
     lastInsertId.finalize;
+    insertBadge.finalize;
+    insertLink.finalize;
+    selectPage.finalize;
 
     db.close;
   }
@@ -152,7 +234,7 @@ unittest {
   auto db = Database("data.db");
 
   // Prepare an SELECT statement for the inserted pages
-  Statement statement = db.prepare("SELECT * FROM pages");
+  Statement statement = db.prepare("SELECT * FROM pages WHERE id = 1 LIMIT 1");
 
   int found = 0;
   foreach (Row row; statement.execute) {
@@ -190,7 +272,24 @@ unittest {
   keywordLinks.should.containOnly([ "1.1", "1.2" ]);
 
   /// Badges
+  statement = db.prepare("SELECT * FROM badges");
 
-  /// Relations
+  string[] strBadges;
+  foreach (Row row; statement.execute) {
+    strBadges ~= row["pageId"].as!string ~ " " ~ row["type"].as!string ~ " " ~ row["signature"].as!string;
+  }
 
+  statement.finalize;
+  strBadges.should.containOnly([ "1 1 [1, 2, 3]" ]);
+
+  /// Page links
+  statement = db.prepare("SELECT * FROM links");
+
+  string[] strLinks;
+  foreach (Row row; statement.execute) {
+    strLinks ~= row["pageId"].as!string ~ "." ~ row["destinationId"].as!string;
+  }
+
+  statement.finalize;
+  strLinks.should.containOnly([ "1.2", "1.3" ]);
 }
